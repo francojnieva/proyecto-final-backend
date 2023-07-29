cda3e5755d2fdb2e2632481db805dcccf2f4f9f9
francojnieva <franco97nieva@hotmail.com>
2023-07-29 15:42:15 -0300

    feat: add collection to the database called about-us, add new route and controller to get the data from the collection

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -2,14 +2,26 @@ import { ContactModel } from "../schemas/contactSchema.js";
 
 
 export const contactForm = async (req, res) => {
-    const { name, lastname, email, message } = req.body
-    const contact = new ContactModel({ name, lastname, email, message })
-  
-    try {
-      await contact.save();
-      res.status(200).json({ message: "Datos guardados correctamente." })
-    } catch (error) {
-      console.error("Error al guardar los datos:", error);
-      res.status(500).json({ message: "Ha ocurrido un error al guardar los datos." })
-    }
+  const { name, lastname, email, message } = req.body
+  const contact = new ContactModel({ name, lastname, email, message })
+
+  try {
+    await contact.save();
+    res.status(200).json({ message: "Datos guardados correctamente." })
+  } catch (error) {
+    console.error("Error al guardar los datos:", error);
+    res.status(500).json({ message: "Ha ocurrido un error al guardar los datos." })
+  }
+}
+
+export const aboutUs = async (req, res) => {
+  try {
+    const db = mongoose.connection
+    const collection = db.collection("about-us");
+    const aboutUsData = await collection.find({}).toArray()
+    res.json(aboutUsData)
+  } catch (error) {
+    console.error("Error al obtener los datos:", error)
+    res.status(500).json({ message: "Ha ocurrido un error al obtener los datos." })
+  }
 }
--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -1,8 +1,10 @@
 import express from "express"
 import { contactForm } from "../controllers/auth.controller.js";
+import { aboutUs } from "../controllers/auth.controller.js";
 
 const router = express.Router();
 
 router.post("/contact", contactForm)
+router.get("/about-us", aboutUs) 
 
 export default router
