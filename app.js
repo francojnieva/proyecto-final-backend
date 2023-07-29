3ba11d42730c54eab06109b18568e1394d80bf74
francojnieva <franco97nieva@hotmail.com>
2023-07-29 18:17:18 -0300

    feat: add two profile images in images folder

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 import express from "express"
 import cors from "cors"
 import router from "./routes/auth.routes.js"
+import path from "path"
 
 const app = express()
 app.use(express.json())
 app.use(cors())
 app.use(router)
+app.use("/images", express.static(path.join(__dirname, "images")))
 
 export default app
