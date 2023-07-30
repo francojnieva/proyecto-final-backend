18af361042c525e475e0a1ecd9c6ca04c58f3faa
francojnieva <franco97nieva@hotmail.com>
2023-07-29 21:28:53 -0300

    feat: add two imports from url and mongoose and add a new profile image

--- app.js
+++ app.js
@@ -2,11 +2,16 @@ import express from "express"
 import cors from "cors"
 import router from "./routes/auth.routes.js"
 import path from "path"
+import { fileURLToPath } from "url";
 
 const app = express()
 app.use(express.json())
 app.use(cors())
 app.use(router)
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 app.use("/images", express.static(path.join(__dirname, "images")))
 
 export default app
--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -1,4 +1,5 @@
 import { ContactModel } from "../schemas/contactSchema.js";
+import mongoose from "mongoose";
 
 
 export const contactForm = async (req, res) => {
