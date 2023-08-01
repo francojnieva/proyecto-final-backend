03ddaf22d64d2a386817ba17cdc0074a500d184b
francojnieva <franco97nieva@hotmail.com>
2023-08-01 02:06:23 -0300

    feat: add new route and controller referring to testimonials

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -26,3 +26,15 @@ export const aboutUs = async (req, res) => {
     res.status(500).json({ message: "Ha ocurrido un error al obtener los datos." })
   }
 }
+
+export const testimony = async (req, res) => {
+  try {
+    const db = mongoose.connection
+    const collection = db.collection("testimony");
+    const aboutUsData = await collection.find({}).toArray()
+    res.json(aboutUsData)
+  } catch (error) {
+    console.error("Error al obtener los datos:", error)
+    res.status(500).json({ message: "Ha ocurrido un error al obtener los datos." })
+  }
+}
--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -1,10 +1,12 @@
 import express from "express"
 import { contactForm } from "../controllers/auth.controller.js";
 import { aboutUs } from "../controllers/auth.controller.js";
+import { testimony } from "../controllers/auth.controller.js";
 
 const router = express.Router();
 
 router.post("/contact", contactForm)
 router.get("/about-us", aboutUs) 
+router.get("/testimony", testimony ) 
 
 export default router
